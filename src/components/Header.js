bbd1cd10f4aee1e5633c1fbeb9bb9092d994202c
Emil <emil.a.mork@gmail.com>
2020-06-16 14:50:29 +0200

    Fix header link

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default () => (
   <header className="text-gray-700 body-font">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <a
-        href="/"
+        href="/recoil-foodtalk-demo"
         className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
       >
         <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
