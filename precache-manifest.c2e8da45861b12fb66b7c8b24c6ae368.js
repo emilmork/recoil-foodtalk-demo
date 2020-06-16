d8b3af955dee5b9ca273fcd1ff8ae4fa7bb736f5
emilmork <emilmork@users.noreply.github.com>
2020-06-16 12:09:39 +0000

    deploy: 3027d539d7df22cb89a56e731e451d59f9349a9d

---
+++ precache-manifest.c2e8da45861b12fb66b7c8b24c6ae368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed38bb8ad3b85b461d48de0a76e156b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b74e6c01fe08a261e529",
+    "url": "/static/css/main.8f623235.chunk.css"
+  },
+  {
+    "revision": "5fa9bbf4df78ebac60d8",
+    "url": "/static/js/2.8ef2803f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8ef2803f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b74e6c01fe08a261e529",
+    "url": "/static/js/main.02e5daec.chunk.js"
+  },
+  {
+    "revision": "9661aee6f990f742fda2",
+    "url": "/static/js/runtime-main.3a4217f9.js"
+  }
+]);
